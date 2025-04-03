f4a8cdaff513759e8dc84e562d7e36e4feba8201
leleterrete <leticia.t.souza7@aluno.senai.br>
2025-04-03 10:14:44 -0300

    Desafio nomes

--- main.go
+++ main.go
@@ -6,9 +6,15 @@ func main(){
 	var ages = [5]int{17,16,20,40,50}
 	fmt.Println(ages)
 	// slice
-	var score= []int{2,3,4,5,6}
-	fmt.Println(score)
-	fmt.Println(score, len(score), cap(score))
-	score = append(score,1,7,8)
-	fmt.Println(score, len(score), cap(score))
-	}
+	var nomes =[]string {"Leticia","Yasmim","Amanda", "Ana","Fabiano" }
+	fmt.Println(nomes)
+	fmt.Println(nomes, len(nomes), cap(nomes))
+	rangeOne := nomes[:2]
+	fmt.Println(rangeOne)	
+	rangeTwo := nomes[3:]
+	fmt.Println(rangeTwo)
+	rangeThree := (nomes[2])
+	fmt.Println(rangeThree)
+
+}
+
